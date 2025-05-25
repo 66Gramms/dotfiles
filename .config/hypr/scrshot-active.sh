cb8df675183dd35d7655c3acd0b391fd15415f2d
66Gramms <meszarosricsi01@gmail.com>
2025-05-25 22:00:36 +0200

    Fix screenshot scripts

--- .config/hypr/scrshot-active.sh
+++ .config/hypr/scrshot-active.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-
-# Define output directory
-output="~/Pictures/Screenshots/active-$(date +%Y-%m-%d_%H-%M-%S).png"
-
+output=~/Pictures/Screenshots/active-$(date +%Y-%m-%d_%H-%M-%S).png
 # Get the geometry of the focused window
 geometry=$(hyprctl activewindow -j | jq -r '"\(.at[0]),\(.at[1]) \(.size[0])x\(.size[1])"')
 
 # Take screenshot of focused window
-grim -g "$geometry" "$output" && wl-copy < "$output"
+grim -g "$geometry" $output && wl-copy < $output
 
 # Send notification
 notify-send -t 3000 "Grim" "Screenshot saved to $output and copied to clipboard"
